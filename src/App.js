78e7a871cfea28d3ada38ae14ec74ae1c43c690e
seo-yeongjun <zanygeek8371@gmail.com>
2022-09-04 16:32:48 +0900

    로그인, 조인, 메인 페이지 틀 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {Outlet, Route, Routes} from "react-router";
+import Header from './component/Header'
+import Footer from './component/Footer'
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import JoinPage from "./pages/JoinPage";
+
+
+const Layout = () =>{
+  return(
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+  )
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<MainPage />}></Route>
+                <Route path="login" element={<LoginPage />}></Route>
+                <Route path="join" element={<JoinPage />}></Route>
+            </Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/pages/JoinPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JoinPage = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default JoinPage
