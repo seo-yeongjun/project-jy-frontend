d5372dc4dbc8d93025e036715a90b46f85c11815
Jeongjaesang <86584123+Jeongjaesang@users.noreply.github.com>
2022-09-04 20:06:26 +0900

    Header.js  Header.css
    .

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,31 @@
 import React from 'react';
-
+import header from '../header.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faApple } from "@fortawesome/free-brands-svg-icons";
+import { FaSearch } from 'react-icons/fa';
+import { FaSignInAlt} from "react-icons/fa";
+import { FaUser} from "react-icons/fa";
 const Header = () => {
     return(
-        <div></div>
+        <div className='header'>
+            <span> logo </span>
+            <div className='searchInput'>
+                <input className='searchInput_input'/>
+                <button className='searchInput-btn'> <FaSearch /></button>
+            </div>
+                
+            <div className='userSection'>
+                <div className='signInBtn'>
+                    {/* <FaUser className='FaUser' /> */}
+                    <span>로그인</span>  
+                </div>
+
+                <div className='signUpBtn'>
+                    {/* <FaSignInAlt className='FaSignInAlt' /> */}
+                    <span>회원가입</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
