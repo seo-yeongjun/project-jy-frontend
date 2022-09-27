a14a862b8c5a19f449e4546f6a9a7ee19e8759c8
seo-yeongjun <zanygeek8371@gmail.com>
2022-09-27 23:32:28 +0900

    footer git url, 연락처 추가

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Footer.css'
 import {FaGithub} from 'react-icons/fa';
+import {useNavigate} from "react-router";
 
 const Footer = () => {
+    const navigate = useNavigate();
     return (
         // Footer Component with logo and social media links
         <div className="footer">
-            <div className="footer-logo">
+            <div className="footer-logo hover:cursor-pointer" onClick={() => navigate('/')}>
                 <img src="/img/logo-square.svg" alt="logo"/>
             </div>
 
@@ -14,8 +16,17 @@ const Footer = () => {
                 <span className="footer-text-span">Designed and developed by </span>
             </div>
             <div className="footer-social">
-                <span className="footer-social-media-icon"><FaGithub size="30px" color="#999" className="icon"/><span>seo-yeongjun</span></span>
-                <span className="footer-social-media-icon"><FaGithub size="30px" color="#999" className="icon"/><span>jeong-jaesang</span></span>
+                <a href='https://github.com/seo-yeongjun' target='_blank' rel='noopener noreferrer'
+                   className="footer-social-media-icon"><FaGithub size="30px" color="#999"
+                                                                  className="icon"/><span>seo-yeongjun</span></a>
+                <a href='https://github.com/Jeongjaesang' target='_blank' rel='noopener noreferrer'
+                   className="footer-social-media-icon"><FaGithub size="30px" color="#999"
+                                                                  className="icon"/><span>jeong-jaesang</span></a>
+            </div>
+            <div className='footer-text mt-2'>
+                <div className="footer-contact">
+                    <span className="footer-text-span">Contact us: 010-6735-6203</span>
+                </div>
             </div>
         </div>
     )
